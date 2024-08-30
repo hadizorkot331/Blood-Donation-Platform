952dd65b0c13d7e34d231d6153b3a7876182523a
Hadi Zorkot <hadizorkot.hadi@gmail.com>
2024-08-30 21:47:53 +0300

    Fixed settings path

--- bloodDonationPlatform/bloodDonationPlatform/wsgi.py
+++ bloodDonationPlatform/bloodDonationPlatform/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bloodDonationPlatform.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "bloodDonationPlatform.bloodDonationPlatform.settings"
+)
 
 application = get_wsgi_application()
